150dba9e8aca2f0669c719196dee4b13eef79a84
Rebecca Strauss <rstrauss127@gmail.com>
2018-08-31 22:07:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,11 +29,12 @@ class Transfer
   end
 
   def reverse_transfer
-    @sender.balance += @amount
-    @receiver.balance -= @amount
-    @status = "reversed"
-
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
     end
+  end
 
 
   end
